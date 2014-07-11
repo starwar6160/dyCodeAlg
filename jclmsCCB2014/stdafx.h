809eb2cc25c455379072763ca4f48330c1ad31a8
Zhou Wei <zhouwei6160@gmail.com>
2014-07-11 14:13:30 +0800

    用提取的部分windows.h中的定义，取消了windows.h的需要，以便/Za编译通过。不过尚未完全/Za

--- jclmsCCB2014/stdafx.h
+++ jclmsCCB2014/stdafx.h
@@ -9,7 +9,7 @@
 
 #define WIN32_LEAN_AND_MEAN             //  从 Windows 头文件中排除极少使用的信息
 // Windows 头文件:
-#include <windows.h>
+//#include <windows.h>
 
 // TODO: 在此处引用程序需要的其他头文件
 #include <ctime>
@@ -22,3 +22,25 @@ using std::string;
 using std::endl;
 using std::vector;
 using std::map;
+
+
+//////////////////////替换windows.h中用到的部分定义开始///////////////////////////////
+//如果有新的部分定义，从windows.h中复制过来
+typedef int                 BOOL;
+#define WINAPI      __stdcall
+#define APIENTRY    WINAPI
+#define DECLARE_HANDLE(name) struct name##__{int unused;}; typedef struct name##__ *name
+DECLARE_HANDLE(HINSTANCE);
+typedef HINSTANCE HMODULE;      /* HMODULEs can be used in place of HINSTANCEs */
+typedef unsigned long       DWORD;
+typedef void *            *LPVOID;	//此处和windef.h不太一样，去掉了过时的far关键字
+
+#define DLL_PROCESS_ATTACH   1    
+#define DLL_THREAD_ATTACH    2    
+#define DLL_THREAD_DETACH    3    
+#define DLL_PROCESS_DETACH   0    
+
+#define FALSE 0
+#define TRUE 1
+
+//////////////////////替换windows.h中用到的部分定义结束///////////////////////////////
---
+++ jclmsCCB2014/zwstdafx.h
@@ -0,0 +1,9 @@
+#ifndef zwstdafx_h__
+#define zwstdafx_h__
+
+#include <string>
+using std::string;
+
+
+#endif // zwstdafx_h__
+
