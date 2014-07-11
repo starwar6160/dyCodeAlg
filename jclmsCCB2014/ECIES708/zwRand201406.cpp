ee48c723940323191b2fcb5e50bd5547586e004e
Zhou Wei <zhouwei6160@gmail.com>
2014-07-11 16:37:02 +0800

    接收并Merge万敏的随机数生成器实现

--- jclmsCCB2014/ECIES708/zwRand201406.cpp
+++ jclmsCCB2014/ECIES708/zwRand201406.cpp
@@ -1,31 +1,30 @@
 #include "sha2.h"
-#undef WIN32
 #ifdef WIN32
-#include <Windows.h>
-#endif	//end of WIN32
-#include <ctime>
+unsigned int RdtscRand()
+{
+	unsigned int iRand = 0;
+	__asm
+	{
+		rdtsc;0
+			mov iRand, eax;
+	}
+	return iRand;
+}
+#else	//ARM
+extern unsigned int RdtscRand();
+//如果是ARM，请使用其硬件随机数生成器写一个如上原型的函数，放在单独文件中；
+#endif // WIN32
 
 void zwRandSeedGen603(char *randBuf,const int randBufLen)
 {
-#ifdef WIN32
-	int rndCount=(randBufLen)/sizeof(LARGE_INTEGER);
-	LARGE_INTEGER rnd;
-#else
-	int rndCount=(randBufLen)/sizeof(int);
-	int rnd;
-#endif // WIN32
+	int rndCount=(randBufLen)/sizeof(unsigned int);
+	unsigned int rnd;
 	sha512_ctx shactx;
 	sha512_init(&shactx);
 	for (int i=0;i<rndCount;i++)
 	{
-#ifdef WIN32
-		QueryPerformanceCounter(&rnd);
-		Sleep(1);	//利用进程调度的不确定性增加随机性
-#else	
-		rnd=clock();
-#endif // WIN32
-		//*(LONGLONG *)(randBuf)=rnd.QuadPart;
-		sha512_update(&shactx,(unsigned char *)&rnd,sizeof(rnd));		
+		rnd = RdtscRand();
+		sha512_update(&shactx,(unsigned char *)&rnd,sizeof(rnd));
 	}
 	unsigned char sha512out[SHA512_DIGEST_SIZE];
 	sha512_final(&shactx,sha512out);
@@ -34,5 +33,5 @@ void zwRandSeedGen603(char *randBuf,const int randBufLen)
 	{
 		randBuf[i]=sha512out[(i)%SHA512_DIGEST_SIZE];
 	}
-}
 
+}
