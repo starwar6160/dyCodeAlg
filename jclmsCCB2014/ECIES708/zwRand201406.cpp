06c7f53462b91f86141984585a8c3bd28dd7ae12
Zhou Wei <zhouwei6160@gmail.com>
2014-07-11 14:55:35 +0800

    zwRand201406.cpp改用/Za了

--- jclmsCCB2014/ECIES708/zwRand201406.cpp
+++ jclmsCCB2014/ECIES708/zwRand201406.cpp
@@ -1,21 +1,31 @@
-
 #include "sha2.h"
+#undef WIN32
 #ifdef WIN32
 #include <Windows.h>
 #endif	//end of WIN32
+#include <ctime>
 
 void zwRandSeedGen603(char *randBuf,const int randBufLen)
 {
+#ifdef WIN32
 	int rndCount=(randBufLen)/sizeof(LARGE_INTEGER);
 	LARGE_INTEGER rnd;
+#else
+	int rndCount=(randBufLen)/sizeof(int);
+	int rnd;
+#endif // WIN32
 	sha512_ctx shactx;
 	sha512_init(&shactx);
 	for (int i=0;i<rndCount;i++)
 	{
+#ifdef WIN32
 		QueryPerformanceCounter(&rnd);
-		//*(LONGLONG *)(randBuf)=rnd.QuadPart;
-		sha512_update(&shactx,(unsigned char *)&rnd,sizeof(rnd));
 		Sleep(1);	//利用进程调度的不确定性增加随机性
+#else	
+		rnd=clock();
+#endif // WIN32
+		//*(LONGLONG *)(randBuf)=rnd.QuadPart;
+		sha512_update(&shactx,(unsigned char *)&rnd,sizeof(rnd));		
 	}
 	unsigned char sha512out[SHA512_DIGEST_SIZE];
 	sha512_final(&shactx,sha512out);
