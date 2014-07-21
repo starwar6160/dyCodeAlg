8e29981d1ed0d7fa660923baebee31dacf9452b0
Zhou Wei <zhouwei6160@gmail.com>
2014-07-21 22:06:36 +0800

    gtest改为MulThread DLL引用了

--- ccbTest702/ccbTest702.cpp
+++ ccbTest702/ccbTest702.cpp
@@ -14,10 +14,10 @@ void myBinString2intTest1();
 
 #ifdef _DEBUG
 #pragma comment(lib,"gtestd.lib")
-#pragma comment(lib,"gtest_maind.lib")
+#pragma comment(lib,"gtest_main-mdd.lib")
 #else
 #pragma comment(lib,"gtest.lib")
-#pragma comment(lib,"gtest_main.lib")
+#pragma comment(lib,"gtest_main-md.lib")
 #endif // _DEBUG
 
 int _tmain(int argc, _TCHAR* argv[])
