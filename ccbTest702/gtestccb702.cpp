ffffc0b738fe1250bd3639958bd23c6d0e10c9d4
Zhou Wei <zhouwei6160@gmail.com>
2014-07-23 11:13:50 +0800

    ECIES测试改为类成员变量来测试了

--- ccbTest702/gtestccb702.cpp
+++ ccbTest702/gtestccb702.cpp
@@ -26,41 +26,46 @@ TEST(FooTest, HandleNoneZeroInput)
 #endif // _DEBUG722
 
 
-
-char s_priKey[ZW_ECIES_PRIKEY_LEN];
-char s_pubKey[ZW_ECIES_PUBKEY_LEN];
-char *s_PlainText="zhouweiPlaintext20140722.1534Test";
-char s_syncKey[ZW_ECIES_ENCSYNCKEY_LEN];
-char s_hash[ZW_ECIES_HASH_LEN];
-char s_crypt[ZW_ECIES_MESSAGE_MAXLEN];
-
 class ECIES_Test : public testing::Test {
 	// Some expensive resource shared by all tests.
 	//	static T* shared_resource_;
+public:
+	//注意要使得一个测试集合中的前后相关的各个测试共用变量
+	//就需要设置变量为static才可以在多个测试中保留值
+	//不然就会在每个测试新建一个新的类实例，变量被重新初始化，中间值不保留
+	static char s_priKey[ZW_ECIES_PRIKEY_LEN];
+	static char s_pubKey[ZW_ECIES_PUBKEY_LEN];
+	static char s_syncKey[ZW_ECIES_ENCSYNCKEY_LEN];
+	static char s_hash[ZW_ECIES_HASH_LEN];
+	static char s_crypt[ZW_ECIES_MESSAGE_MAXLEN];
+	static char *s_PlainText;
 
 protected:
 	static void SetUpTestCase() {
-		//shared_resource_ = new ;
-		memset(s_priKey,0,sizeof(s_priKey));
-		memset(s_pubKey,0,sizeof(s_pubKey));
-		memset(s_syncKey,0,sizeof(s_syncKey));
-		memset(s_hash,0,sizeof(s_hash));
-		memset(s_crypt,0,sizeof(s_crypt));
-		
+		//shared_resource_ = new ;		
 	}
 	static void TearDownTestCase() {
 		//delete shared_resource_;
 		//shared_resource_ = NULL;
 	}
 };
+char ECIES_Test::s_priKey[ZW_ECIES_PRIKEY_LEN];
+char ECIES_Test::s_pubKey[ZW_ECIES_PUBKEY_LEN];
+char ECIES_Test::s_syncKey[ZW_ECIES_ENCSYNCKEY_LEN];
+char ECIES_Test::s_hash[ZW_ECIES_HASH_LEN];
+char ECIES_Test::s_crypt[ZW_ECIES_MESSAGE_MAXLEN];
+char * ECIES_Test::s_PlainText;
+
+
 
 TEST_F(ECIES_Test,NormalKeyPairGen)
 {
-	//myJcLockInputTest1();
+	s_PlainText="zhouweiPlaintext20140722.1534Test";
+	memset(s_priKey,0,sizeof(s_priKey));
+	memset(s_pubKey,0,sizeof(s_pubKey));
+
 	EXPECT_GT(ZW_ECIES_PUBKEY_LEN,0);
 	EXPECT_LT(ZW_ECIES_PUBKEY_LEN,100);
-	//ZWECIES_API int zwEciesKeyPairGen( const char *password,char *outPriKeyStr,const int priLen,
-	//	char *outPublicKeyStr ,const int pubLen);
 	int keygenResult=zwEciesKeyPairGen("password",s_priKey,ZW_ECIES_PRIKEY_LEN,s_pubKey,ZW_ECIES_PUBKEY_LEN);
 	EXPECT_EQ(keygenResult,ECIES_SUCCESS);
 	EXPECT_GT(strlen(s_priKey),0);
@@ -73,6 +78,9 @@ cout<<"s_pubKey=\t"<<s_pubKey<<endl;
 
 TEST_F(ECIES_Test,NormalEnc)
 {
+	memset(s_syncKey,0,sizeof(s_syncKey));
+	memset(s_hash,0,sizeof(s_hash));
+	memset(s_crypt,0,sizeof(s_crypt));
 EXPECT_GT(strlen(s_priKey),0);
 EXPECT_GT(strlen(s_pubKey),0);
 int eciesEncRet=zwEciesEncrypt(s_pubKey,s_PlainText,s_syncKey,sizeof(s_syncKey),
@@ -87,13 +95,6 @@ cout<<"s_hash=\t"<<s_hash<<endl;
 cout<<"s_crypt=\t"<<s_crypt<<endl;
 #endif // _DEBUG
 }
-
-//ECIES解密：
-//prikeyStr：编码过的私钥	outPlainText：输出明文缓冲区	plainLen：明文缓冲区长度
-//EncryptedSyncKeyStr，MsgHashStr，CryptedTextStr：含义同加密输出的3个项目
-//ZWECIES_API BOOL zwEciesDecrypt(const char *prikeyStr,char *outPlainText,const int plainLen, 
-//	const char *EncryptedSyncKeyStr,const char *MsgHashStr,const char *CryptedTextStr);
-
 
 TEST_F(ECIES_Test,NormalDec)
 {
