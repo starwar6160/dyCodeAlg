6c6e6730df93152130cb6e498c1d1269316dd808
Zhou Wei <zhouwei6160@gmail.com>
2014-12-31 15:45:05 +0800

    密盒测试C#代码段迁入jclmsCCB2014了

--- cstest702/Program.cs
+++ cstest702/Program.cs
@@ -728,6 +728,75 @@ namespace cstest702
             Console.Out.WriteLine("########################################################################");
         }
 
+        private static void secTest1()
+        {
+            for (int i = 0; i < 1; i++)
+            //while(true)
+            {
+                jclms.JcSecBox secBox = new JcSecBox();
+                Console.Out.WriteLine("Secret Box Open###########################################################");
+                //打开密盒                
+                int status =
+                    secBox.SecboxAuth();
+
+                if (0 == status)
+                {
+                    Console.Out.WriteLine("Good Secret Box");
+                }
+                if (1 == status)
+                {
+                    Console.Out.WriteLine("Fake Secret Box");
+                    continue;
+                }
+                //////////////////////////////////////////////////////////
+                //随便用一段比较长的文字经过base64编码形成的下面这段有待写入的base64数据
+                //实践中，可以用二进制数据编码之后成为base64字符串写入；
+                //第二个参数是索引号，大致上是0到10左右，具体还得和赵工确认
+                //第三个参数，也就是数据，大体上可以达到最大400多个字节，具体多少还得和赵工确认
+                const String myLongB64Str1 = "12345678";
+                //"emhvdXdlaXRlc3RPdXRwdXREZWJ1Z1N0cmluZ0FuZEppbkNodUVMb2NraW5kZXg9MFRvdGFsQmxvY2s9MkN1ckJsb2NrTGVuPTU4U2VkaW5nIERhdGEgQmxvY2sgIzBSZWNldmVkIERhdGEgRnJvbSBKQ0VMb2NrIGlzOg==";
+                //通过句柄，索引号，读取密盒数据，返回的也是Base64编码过的字符串，解码后可能是文本，也可能是二进制数据
+
+
+                String recvFromSecBox = secBox.SecboxReadData(2);
+                Console.Out.WriteLine("Secret Box ReadData is {0}", recvFromSecBox);
+                secBox.SecboxWriteData(2, myLongB64Str1);
+                Console.Out.WriteLine("Secret Box WriteData is {0}", myLongB64Str1);
+                secBox.SecboxReadData(2);
+                Console.Out.WriteLine("Secret Box ReadData2 is {0}", recvFromSecBox);
+                //Console.Out.WriteLine("Secret Box ReadData");
+
+                Console.Out.WriteLine("WAIT 4 SECONDS FOR PLUG OUT/IN SECRET BOX");
+                //System.Threading.Thread.Sleep(ZWPAUSE*5);
+            }
+        }
+
+        private static void secTest2()
+        {
+            Console.Out.WriteLine("Secret Box Authentic Test");
+            for (int i = 0; i < 64 * 3; i++)
+            {
+                jclms.JcSecBox secBox = new JcSecBox();
+                //打开密盒                
+                int status =
+                    secBox.SecboxAuth();
+
+                if (0 == status)
+                {
+                    Console.Out.Write(".");
+                }
+                if (1 == status)
+                {
+                    Console.Out.WriteLine("FAKE ");
+                    continue;
+                }
+                if (i > 0 && i % 32 == 0)
+                {
+                    Console.Out.Write("{0}\t", i);
+                }
+                System.Threading.Thread.Sleep(50);
+            }
+        }
 
 ///////////////////////////////////////////////////////////////////////////////////////////
     }   //class Program
