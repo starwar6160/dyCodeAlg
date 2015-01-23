ced23f602db09c9cef29e76e7c8bbcbb5525f227
zhouwei6160 <zhouwei6160@gmail.com>
2015-01-23 10:29:58 +0800

    stm32test实现了ECIES密钥对生成

--- stm32Test/stdafx.h
+++ stm32Test/stdafx.h
@@ -13,3 +13,6 @@
 
 
 // TODO: 在此处引用程序需要的其他头文件
+#include <iostream>
+using std::cout;
+using std::endl;
--- stm32Test/stm32Test.cpp
+++ stm32Test/stm32Test.cpp
@@ -2,10 +2,30 @@
 //
 
 #include "stdafx.h"
+#include "zwEcies529.h"
 
+void myECIES_KeyGenTest123(void)
+{
+
+		int hd = 0;
+		hd = EciesGenKeyPair();
+		//EXPECT_NE(hd, 0);
+		char pubkey[ZW_ECIES_PUBKEY_LEN];
+		char prikey[ZW_ECIES_PRIKEY_LEN];
+		memset(pubkey, 0, sizeof(pubkey));
+		memset(prikey, 0, sizeof(prikey));
+		strcpy(pubkey, EciesGetPubKey(hd));
+		strcpy(prikey, EciesGetPriKey(hd));
+		//EXPECT_GT(strlen(pubkey), 0);
+		//EXPECT_GT(strlen(prikey), 0);
+		cout << "pubkey=\t" << pubkey << endl;
+		cout << "prikey=\t" << prikey << endl;
+		EciesDelete(hd);
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	myECIES_KeyGenTest123();
 	return 0;
 }
 
