e44dff112f0f07847312392bcd5d8ca88ae43f0e
zhouwei6160 <zhouwei6160@gmail.com>
2015-01-26 10:56:14 +0800

    加强注释给孙工的版本

--- stm32Test2/zwECIES/stm32Test2.cpp
+++ stm32Test2/zwECIES/stm32Test2.cpp
@@ -18,27 +18,46 @@ void myECIES_KeyGenTest123(void)
 	//ECIES DEC Text= C# Port Test plain text 20140722.1625
 
 	//一般来说，生成密钥操作做一次就行了，以下的常量密钥对就是某一次生成的结果复制下来的
-	//int hd = 0;
-	//hd = EciesGenKeyPair();
-	//EXPECT_NE(hd, 0);
+#ifdef _DEBUG126
+	//首先用这一段程序生成公钥私钥对，然后保存在内存和FLASH里面，以后别处要用到的时候，就从内存或者FLASH里面取出来使用
+	//注意一个ARM设备基本上运行一次这段程序就行了，一个公钥/私钥对存在于整个ARM设备的生命周期，除非重新初始化才会再次生成
+	int hd = 0;
+	hd = EciesGenKeyPair();
+	const char *myPubKey=EciesGetPubKey(hd);
+	const char *myPriKey=EciesGetPriKey(hd);
+	//如果生成了密钥对，那么别忘了删除，释放内存
+	EciesDelete(hd);
+#endif // _DEBUG126
+	//以下
 	char pubkey[ZW_ECIES_PUBKEY_LEN];
 	char prikey[ZW_ECIES_PRIKEY_LEN];
 	memset(pubkey, 0, sizeof(pubkey));
 	memset(prikey, 0, sizeof(prikey));
+	//以下公钥/私钥对是前面EciesGenKeyPair生成，然后用EciesGetPubKey和EciesGetPriKey取得的，请保存在FLASH里面以备后用
 	strcpy(pubkey,"BFlfjkxoiRZFdjQKa/W1JWBwFx+FPyzcFGqXjnlVzMcvIAQyK3C1Ha+G2uGUM4nX5khPQP5AiPFiCyuH2WxZefg=");
 	strcpy(prikey,"y+tgryY83ibv2RaQeb93a97+JX0/9cpWf4MrmUUtrzs=");
 	printf("pubkey=%s\nprikey=%s\n",pubkey,prikey);
 	//////////////////////////////////////////////////////////////////////////
-	const char *csPlainText =
-		"C# Port Test plain text 20140722.1625";
-	char crypt[ZW_ECIES_CRYPT_TOTALLEN];
-	memset(crypt, 0, sizeof(crypt));
-	strcpy(crypt, EciesEncrypt(pubkey, csPlainText));
-	printf("ECIES ENC Text= %s\n",crypt);
-	strcpy(crypt,EciesDecrypt(prikey, crypt));	
-	printf("ECIES DEC Text= %s\n",crypt);
-	//如果生成了密钥对，那么别忘了删除，释放内存
-	//EciesDelete(hd);
+	//建行的两个用于生成PSK的输入因子，将其拼接在ccbInStr里面
+	const char *ccbInput1="0123456789ABCDEF";
+	const char *ccbInput2="01234ABCDEF56789";
+	char ccbInStr[40];
+	memset(ccbInStr,0,40);
+	strcpy(ccbInStr,ccbInput1);
+	strcat(ccbInStr,ccbInput2);
+	//从ccbInStr生成PSK
+	const char *ccbPSK=zwMergePsk(ccbInStr);
+	printf("from CCB1 %s and CCB2 %s result \nccbPSK=\t%s\n",ccbInput1,ccbInput2,ccbPSK);
+	char ccbActiveInfo[ZW_ECIES_CRYPT_TOTALLEN];
+	memset(ccbActiveInfo, 0, sizeof(ccbActiveInfo));
+	//从PSK和公钥生成激活信息ccbActiveInfo，然后激活信息就可以通过网络传输出去了
+	strcpy(ccbActiveInfo, EciesEncrypt(pubkey, ccbPSK));
+	printf("ccbActiveInfo= %s\n",ccbActiveInfo);
+	//用私钥解开激活信息，获得PSK
+	char dePSK[ZW_ECIES_CRYPT_TOTALLEN];
+	memset(dePSK,0,ZW_ECIES_CRYPT_TOTALLEN);
+	strcpy(dePSK,EciesDecrypt(prikey, ccbActiveInfo));	
+	printf("ccbPSK=\t%s\n",dePSK);
 }
 
 int main(int argc, char * argv[])
