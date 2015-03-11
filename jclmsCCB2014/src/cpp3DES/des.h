eddf64710392895f108268ff7d4b12d3e8686292
Zhou Wei <zhouwei6160@gmail.com>
2015-03-11 10:36:06 +0800

    把3DES代码初步整合为一个源文件，一个头文件的形式了，便于使用

--- jclmsCCB2014/src/cpp3DES/des.h
+++ jclmsCCB2014/src/cpp3DES/des.h
@@ -1,8 +1,7 @@
 #ifndef DES_H
 #define DES_H
 
-#include <cstdint>
-using namespace std;
+#include <stdint.h>
 
 #define ui64 uint64_t
 #define ui32 uint32_t
@@ -33,4 +32,38 @@ private:
     ui64 sub_key[16]; // 48 bits each
 };
 
+class DES3
+{
+public:
+	DES3(ui64 k1, ui64 k2, ui64 k3);
+	ui64 encrypt(ui64 block);
+	ui64 decrypt(ui64 block);
+
+private:
+	DES des1;
+	DES des2;
+	DES des3;
+};
 #endif // DES_H
+
+#ifndef DESCBC_H
+#define DESCBC_H
+
+#include "des.h"
+
+class DESCBC
+{
+public:
+	DESCBC(ui64 key, ui64 iv);
+	ui64 encrypt(ui64 block);
+	ui64 decrypt(ui64 block);
+	void reset();
+
+private:
+	DES des;
+	ui64 iv;
+	ui64 last_block;
+};
+
+#endif // DESCBC_H
+
