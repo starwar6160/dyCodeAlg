3167359be1945c4a2cfc583acaae7c09311fe36a
Zhou Wei <zhouwei6160@gmail.com>
2015-03-18 10:04:31 +0800

    修正了des.h里面包含的类声明导致孙玉龙的C主程序无法包含des.h的问题

--- jclmsCCB2014/src/cpp3DES/des.cpp
+++ jclmsCCB2014/src/cpp3DES/des.cpp
@@ -180,6 +180,63 @@ static const char PBOX[] =
 #endif // DES_DATA_H
 
 
+//#include "des.h"
+
+class DES
+{
+public:
+	DES(ui64 key);
+	ui64 des(ui64 block, bool mode);
+
+	ui64 encrypt(ui64 block);
+	ui64 decrypt(ui64 block);
+
+	static ui64 encrypt(ui64 block, ui64 key);
+	static ui64 decrypt(ui64 block, ui64 key);
+
+protected:
+	void keygen(ui64 key);
+
+	ui64 ip(ui64 block);
+	ui64 fp(ui64 block);
+
+	void feistel(ui32 &L, ui32 &R, ui32 F);
+	ui32 f(ui32 R, ui64 k);
+
+private:
+	ui64 sub_key[16]; // 48 bits each
+};
+
+class DES3
+{
+public:
+	DES3(ui64 k1, ui64 k2, ui64 k3);
+	ui64 encrypt(ui64 block);
+	ui64 decrypt(ui64 block);
+
+private:
+	DES des1;
+	DES des2;
+	DES des3;
+};
+
+
+class DESCBC
+{
+public:
+	DESCBC(ui64 key, ui64 iv);
+	ui64 encrypt(ui64 block);
+	ui64 decrypt(ui64 block);
+	void reset();
+
+private:
+	DES des;
+	ui64 iv;
+	ui64 last_block;
+};
+
+
+
 DES::DES(ui64 key)
 {
     keygen(key);
--- jclmsCCB2014/src/cpp3DES/des.h
+++ jclmsCCB2014/src/cpp3DES/des.h
@@ -1,73 +1,14 @@
-#ifndef DES_H
-#define DES_H
+#ifndef des_h__
+#define des_h__
 
 #include <stdint.h>
+#ifdef  __cplusplus
+extern "C" {
+#endif
 
 #define ui64 uint64_t
 #define ui32 uint32_t
 #define ui8  uint8_t
-
-class DES
-{
-public:
-    DES(ui64 key);
-    ui64 des(ui64 block, bool mode);
-
-    ui64 encrypt(ui64 block);
-    ui64 decrypt(ui64 block);
-
-    static ui64 encrypt(ui64 block, ui64 key);
-    static ui64 decrypt(ui64 block, ui64 key);
-
-protected:
-    void keygen(ui64 key);
-
-    ui64 ip(ui64 block);
-    ui64 fp(ui64 block);
-
-    void feistel(ui32 &L, ui32 &R, ui32 F);
-    ui32 f(ui32 R, ui64 k);
-
-private:
-    ui64 sub_key[16]; // 48 bits each
-};
-
-class DES3
-{
-public:
-	DES3(ui64 k1, ui64 k2, ui64 k3);
-	ui64 encrypt(ui64 block);
-	ui64 decrypt(ui64 block);
-
-private:
-	DES des1;
-	DES des2;
-	DES des3;
-};
-#endif // DES_H
-
-#ifndef DESCBC_H
-#define DESCBC_H
-
-//#include "des.h"
-
-class DESCBC
-{
-public:
-	DESCBC(ui64 key, ui64 iv);
-	ui64 encrypt(ui64 block);
-	ui64 decrypt(ui64 block);
-	void reset();
-
-private:
-	DES des;
-	ui64 iv;
-	ui64 last_block;
-};
-
-#ifdef  __cplusplus
-extern "C" {
-#endif
 
 enum JC3DES_ERROR{
 	JC3DES_OK,	//³É¹¦·µ»Ø
@@ -91,5 +32,4 @@ JC3DES_ERROR zwCCB3DESEncryptDyCode(const char *ccbComm3DESKeyHex,const int dyCo
 }	//extern "C" {
 #endif
 
-#endif // DESCBC_H
-
+#endif // des_h__
