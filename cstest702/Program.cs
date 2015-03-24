2d2c56d748538093aea568888a404c4e8f6f393d
Zhou Wei <zhouwei6160@gmail.com>
2015-03-24 16:34:08 +0800

    密码服务器的C#例子，作为基准

--- cstest702/Program.cs
+++ cstest702/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Security.Cryptography;
 using jclms;
 
 namespace cstest702
@@ -15,7 +16,9 @@ namespace cstest702
         const Int32 validity = 5;
         const int ZW_FAKE_PASS1 = 11111111;
 
-        static void Main(string[] args)
+
+
+        private static void myECIESTestOld1()
         {
             //安全初始化例子
             //myECIEStest();
@@ -31,7 +34,7 @@ namespace cstest702
             int myDyCodePass1 = jclmsCCB2014.csJclmsReqGenDyCode(handle);
             return;
 
-            mySecBoxTest1221();                        
+            mySecBoxTest1221();
             tmyLmsReq2SecBoxEx20141221GenPass1DyCode();
             return;
 
@@ -51,7 +54,6 @@ namespace cstest702
             //第二开锁码验证
             myLmsReq2SecBoxEx20141218VerifyPass2DyCode();
         }
-
         //安全初始化例子
         private static void myECIEStest()
         {
@@ -798,6 +800,73 @@ namespace cstest702
             }
         }
 
+
+        //注意，秘钥和数据，密文都是HEX格式
+        public static string DES3Encrypt(string data, string key)
+        {
+            //建行的扩充秘钥的方法，截取16字节字符串的前8字节作为最后一组8字节秘钥
+            //合计成为24字节也就是192bit秘钥
+            String exKey = key + key.Substring(0, 8);
+            TripleDESCryptoServiceProvider DES =
+                new TripleDESCryptoServiceProvider();
+            //此处建行因为历史原因将错就错的生成Key的方式：要求输入16个字符
+            // (其实不限于HEX，任何可打印字符都可以)，然后把前8个字符再次添加
+            // 到输入的末尾从而16个字符扩展到24个字符也就是192bit，作为3DES
+            // 要求的192bit秘钥；此处的要诀与建行“将错就错”在于：3DES要求的是
+            // 192 bit的二进制内容作为Key，但建行给出的是24个HEX字符，实际
+            // 信息量只有24*4=96bit，而通过把24个HEX字符的二进制形式共192bit
+            // 作为Key，将错就错的满足了3DES对于Key长度的要求；
+            DES.Key = ASCIIEncoding.ASCII.GetBytes(exKey);
+            //此处模式一定要是ECB，一是因为建行本来就指定的ECB模式，二是其他
+            //模式都有一个叫做初始化向量的东西，会使得每次加密结果都不同；
+            DES.Mode = CipherMode.ECB;
+            //注意建行指定的填充模式是“无”，不这么做，加密结果就会不同导致建行无法解密；
+            DES.Padding = PaddingMode.None;
+
+            ICryptoTransform DESEncrypt = DES.CreateEncryptor();
+            //注意此处，数据确实是HEX格式字符串输入，但是要化为二进制格式
+            //去给3DES算法做加密；原来使用ASCIIEncoding.ASCII.GetBytes的做法
+            // 使得加密的明文就是错误，是HEX字符串而非他们所代表的本体二进制
+            // 数据被加密的，自然建行解密肯定失败；
+            byte[] Buffer = strToToHexByte(data);
+            //加密结果这次转化为HEX字符串返回；
+            return byteToHexStr(DESEncrypt.TransformFinalBlock(Buffer, 0, Buffer.Length));
+        }
+
+        //这两个byte和HEX字符串转换函数是我从网上找来的
+        public static byte[] strToToHexByte(string hexString)
+        {
+            hexString = hexString.Replace(" ", "");
+            if ((hexString.Length % 2) != 0)
+                hexString += " ";
+            byte[] returnBytes = new byte[hexString.Length / 2];
+            for (int i = 0; i < returnBytes.Length; i++)
+                returnBytes[i] = Convert.ToByte(hexString.Substring(i * 2, 2), 16);
+            return returnBytes;
+        }
+
+        public static string byteToHexStr(byte[] bytes)
+        {
+            string returnStr = "";
+            if (bytes != null)
+            {
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    returnStr += bytes[i].ToString("X2");
+                }
+            }
+            return returnStr;
+        }
+
+        static void Main(string[] args)
+        {
+            //myECIESTestOld1();
+            //324GZ dyCodeHexStr=3231343336353837   ("21436587")
+            string dyEnc = DES3Encrypt("3231343336353837",
+                "1234567890123456");
+            Console.Out.WriteLine("dyEnc={0}", dyEnc);
+            return;
+        }
 ///////////////////////////////////////////////////////////////////////////////////////////
     }   //class Program
 }   //namespace cstest702
