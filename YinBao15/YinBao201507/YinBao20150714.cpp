99bbdc719fb93c0bdbb6a34d7f2e8f83977a0945
Zhou Wei <zhouwei6160@gmail.com>
2015-07-16 14:46:15 +0800

    分离myybHex2Bin函数，把HEX字符串转换为二进制值的

--- YinBao15/YinBao201507/YinBao20150714.cpp
+++ YinBao15/YinBao201507/YinBao20150714.cpp
@@ -8,87 +8,119 @@ void myybSM3Update(SM3 * ctx, const char *data, const int len);
 
 namespace myYinBao201507{
 	const int ZWHASHLEN=256/8;	//256 bit Hash结果
+	int myBin2Hex(const char *inData,int inLength,char *outHex,int outLength)
+	{
+		assert(NULL!=inData && inLength>0);
+		assert(NULL!=outHex && outLength>0);
+		if (NULL==inData || inLength<=0)
+		{
+			return -1148;
+		}
+		if (NULL==outHex || outLength<=0)
+		{
+			return -1149;
+		}
+		//在这里，static的智能指针本身，应该不是什么问题。20150715.1453，周伟
+		//static shared_ptr<string> rtn(new string);
+		// ZWHASHLEN*2+1	
+		string hexHashStr;
+		for (int i=0;i<inLength;i++)
+		{
+			uint8_t ch=inData[i];
+			char st[3];
+			memset(st,0,3);
+			sprintf(st,"%02X",ch);
+			hexHashStr+=st;
+		}
+		memset(outHex,0,outLength);
+		assert(outLength>=hexHashStr.length());
+		strncpy(outHex,hexHashStr.c_str(),outLength);
+		return 0;
+	}
+	void myybSM3Update(SM3 * ctx, const char *data, const int len)
+	{
+		assert(ctx != NULL);
+		assert(data != NULL);
+		if (NULL==ctx || NULL==data)
+		{
+			return;
+		}
+		assert(ctx->length > 0);
+		assert(len > 0);
+		for (int i = 0; i < len; i++) {
+			SM3_Update(ctx, *(data + i));
+			int ch=*(data + i);
+#ifdef _DEBUG_20150309
+			//我和孙玉龙，又是遇到ARM编译器优化级别0导致SM3算法结果错误的问题.20150309.1546
+			//调试过程中用的代码
+			if (1==G_SM3DATA_TRACK)
+			{
+				printf("%02X ",ch);
+			}	
+#endif // _DEBUG_20150309
+
+		}
+	}
+	//从包含二进制数据的字符串输入，获得一个8位整数的输出
+	unsigned int zwBinString2Int32(const char *data, const int len)
+	{
+		//比1开头的8位数稍微大一些的质数
+		const int dyLow = 10000019;
+		//比9开头的8位数稍微小一些的质数
+		const int dyMod = 89999969;
+		const int dyMul = 257;	//随便找的一个质数作为相乘的因子
+
+		unsigned __int64 sum = 0;
+		for (int i = 0; i < len; i++) {
+			unsigned char t = *(data + i);
+			sum *= dyMul;
+			sum += t;
+		}
+		//这两个数字结合使用，产生肯定是8位数的动态码
+		sum %= dyMod;
+		sum += dyLow;
+		return static_cast<unsigned int>(sum);
+	}
+	int myybHex2Bin(const char *inHexStr,char *outBin,int outLen)
+	{	
+		assert(NULL!=inHexStr);
+		assert(NULL!=outBin &&outLen>0);
+		if (NULL==inHexStr || NULL==outBin || outLen<=0)
+		{
+			return -1429;
+		}
+		int inLen=strlen(inHexStr);
+		assert(inLen>0 && (inLen % 2 ==0));
+		if (inLen<=0 || (inLen % 2==1))
+		{
+			return -1430;
+		}	
+		assert((inLen/2)<=outLen);
+		if ((inLen/2)>outLen)
+		{
+			return -1441;
+		}
+		memset(outBin,0,outLen);
+		for (int i=0;i<inLen/2;i++)
+		{
+			int32_t ch=0;
+			sscanf(inHexStr+i*2,"%02X",&ch);
+			assert(ch>=0 && ch<=255);
+			outBin[i]=ch;
+		}
+		return 0;
+	}
 }	//namespace myYinBao201507{
 
 
 using myYinBao201507::ZWHASHLEN;
 
 ///////////////////////////////COPY FROM JCLMSCCB2014///////////////////////////////////////////
-void myybSM3Update(SM3 * ctx, const char *data, const int len)
-{
-	assert(ctx != NULL);
-	assert(data != NULL);
-	if (NULL==ctx || NULL==data)
-	{
-		return;
-	}
-	assert(ctx->length > 0);
-	assert(len > 0);
-	for (int i = 0; i < len; i++) {
-		SM3_Update(ctx, *(data + i));
-		int ch=*(data + i);
-#ifdef _DEBUG_20150309
-		//我和孙玉龙，又是遇到ARM编译器优化级别0导致SM3算法结果错误的问题.20150309.1546
-		//调试过程中用的代码
-		if (1==G_SM3DATA_TRACK)
-		{
-			printf("%02X ",ch);
-		}	
-#endif // _DEBUG_20150309
 
-	}
-}
 
-//从包含二进制数据的字符串输入，获得一个8位整数的输出
-unsigned int zwBinString2Int32(const char *data, const int len)
-{
-	//比1开头的8位数稍微大一些的质数
-	const int dyLow = 10000019;
-	//比9开头的8位数稍微小一些的质数
-	const int dyMod = 89999969;
-	const int dyMul = 257;	//随便找的一个质数作为相乘的因子
 
-	unsigned __int64 sum = 0;
-	for (int i = 0; i < len; i++) {
-		unsigned char t = *(data + i);
-		sum *= dyMul;
-		sum += t;
-	}
-	//这两个数字结合使用，产生肯定是8位数的动态码
-	sum %= dyMod;
-	sum += dyLow;
-	return static_cast<unsigned int>(sum);
-}
 
-int myBin2Hex(const char *inData,int inLength,char *outHex,int outLength)
-{
-	assert(NULL!=inData && inLength>0);
-	assert(NULL!=outHex && outLength>0);
-	if (NULL==inData || inLength<=0)
-	{
-		return -1148;
-	}
-	if (NULL==outHex || outLength<=0)
-	{
-		return -1149;
-	}
-	//在这里，static的智能指针本身，应该不是什么问题。20150715.1453，周伟
-	//static shared_ptr<string> rtn(new string);
-	// ZWHASHLEN*2+1	
-	string hexHashStr;
-	for (int i=0;i<inLength;i++)
-	{
-		uint8_t ch=inData[i];
-		char st[3];
-		memset(st,0,3);
-		sprintf(st,"%02X",ch);
-		hexHashStr+=st;
-	}
-	memset(outHex,0,outLength);
-	assert(outLength>=hexHashStr.length());
-	strncpy(outHex,hexHashStr.c_str(),outLength);
-	return 0;
-}
+
 
 
 //////////////////////////////////////////////////////////////////////////
@@ -176,13 +208,15 @@ YINBAO15_API int __stdcall zwYinBaoGetHashSM3(const char *inData,const int inLen
 	printf("%s\n",__FUNCTION__);
 	SM3 sm3;
 	SM3_Init(&sm3);
-	myybSM3Update(&sm3, inData,inLength);
+	myYinBao201507::myybSM3Update(&sm3, inData,inLength);
 	SM3_Final(&sm3, outHashTmp);
 
-	myBin2Hex(outHashTmp,ZWHASHLEN,outHash256,ZWHASHLEN*2+1);
+	myYinBao201507::myBin2Hex(outHashTmp,ZWHASHLEN,outHash256,ZWHASHLEN*2+1);
 
 	return 0;
 }
+
+
 
 YINBAO15_API int __stdcall zwYinBaoHash2Code( const char *inHexStr )
 {	
@@ -192,14 +226,7 @@ YINBAO15_API int __stdcall zwYinBaoHash2Code( const char *inHexStr )
 		return -1708;
 	}
 	char inHashBin256[ZWHASHLEN];
-	memset(inHashBin256,0,ZWHASHLEN);
-	for (int i=0;i<ZWHASHLEN;i++)
-	{
-		int32_t ch=0;
-		sscanf(inHexStr+i*2,"%02X",&ch);
-		assert(ch>=0 && ch<=255);
-		inHashBin256[i]=ch;
-	}
-	int ybn=zwBinString2Int32(inHashBin256,ZWHASHLEN);
+	myYinBao201507::myybHex2Bin(inHexStr,inHashBin256,ZWHASHLEN);
+	int ybn=myYinBao201507::zwBinString2Int32(inHashBin256,ZWHASHLEN);
 	return ybn;
 };
