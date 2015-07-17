e26cb5a12740fa6c1e2b78540443b0b303b952bb
Zhou Wei <zhouwei6160@gmail.com>
2015-07-17 09:28:23 +0800

    函数名，参数名字改为更符合规范的

--- YinBao15/YinBao15.h
+++ YinBao15/YinBao15.h
@@ -30,15 +30,15 @@ YINBAO15_API int fnYinBao15(void);
 
 #ifdef _DEBUG_20150715
 YINBAO15_API void __stdcall zwYinBaoGetHash(const char *inData,const int inLength,char* outHash256);
-//zwYinBaoHash2Code
-YINBAO15_API int __stdcall zwYinBaoHash2Code(const char *inData);
-YINBAO15_API const char * __stdcall zwYinBaoGetHashSM3(const char *inData,const int inLength);
+//jcYinBaoHash2Code
+YINBAO15_API int __stdcall jcYinBaoHash2Code(const char *inData);
+YINBAO15_API const char * __stdcall jcYinBaoGetHashSM3(const char *inData,const int inLength);
 #endif // _DEBUG_20150715
 
 //默认输出256bit的HASH，无论是SM3还是SHA256，对于我们的用途肯定够用了
-YINBAO15_API int __stdcall zwYinBaoGetHashSM3(const char *inData,const int inLength,char* &outHash256);
+YINBAO15_API int __stdcall jcYinBaoGetHashSM3(const char *inData,const int inLength,char* &outHash256);
 //输入HEX字符串，必须是双数长度，以及要求的位数,可以用6，8，10，12位，如果输入位数错误，默认8位
-YINBAO15_API int64_t __stdcall zwYinBaoHash2Code( const char *inHexStr,int CodeLen );
+YINBAO15_API int64_t __stdcall jcYinBaoHash2Code( const char *inHexStr,int inCodeLen );
 
 
 #ifdef  __cplusplus
